3e9d45cab1102c736d8fa5b3beac1549e693b880
BillLin1209 <zzx123bill@gmail.com>
2021-05-19 00:02:02 +0800

    1. excluding log4j dependency in pom.xml 2. adjust ExceptionHandler

--- src/main/java/com/bill/crm/controller/ExceptionHandler.java
+++ src/main/java/com/bill/crm/controller/ExceptionHandler.java
@@ -1,4 +1,16 @@
 package com.bill.crm.controller;
 
+import com.bill.crm.vo.response.ResponseOneVo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
+@ControllerAdvice
 public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
+    public final ResponseEntity<ResponseOneVo> handleDemoException(Exception e) {
+        e.printStackTrace();
+        return ResponseEntity.badRequest().body(ResponseOneVo.error());
+    }
 }
